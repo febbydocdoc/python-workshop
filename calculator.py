08fb51cd0c6985b3db354e094e1a93571061c455
daffan4567 <88191436+daffan4567@users.noreply.github.com>
2021-08-21 15:24:34 +0700

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,1 +1,18 @@
+number1 = int(input('masukkan angka pertama: '))
+operator = input('pilih operator (+, -, /, *): ')
+number2 = int(input('masukkan angka kedua: '))
 
+result = 0
+if operator == '+':
+    result = number1 + number2
+elif operator == '-':
+    result = number1 - number2
+elif operator == '/':
+    result = number1 / number2
+elif operator == '*':
+    result = number1 / number2
+else:
+    print('invalid operatior!')
+
+if result is not None:
+    print(f'{number1} {operator} {number2} = {result}')
