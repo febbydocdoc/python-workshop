a2aad1c9750d4a24840db6909c145a762a9829d2
daffan4567 <88191436+daffan4567@users.noreply.github.com>
2021-08-22 14:19:52 +0700

    Revisi

--- Quiz Function1 Exercise 2_Muhammad Daffan Al Farizi.py
+++ Quiz Function1 Exercise 2_Muhammad Daffan Al Farizi.py
@@ -1,12 +1,19 @@
+def check_upper_lower(str):
+    upper = 0
+    lower = 0
+
+    for i in range(len(str)):
+
+        if(str[i] >= 'a' and str[i] <= 'z'):
+            lower += 1
+
+        elif(str[i] >= 'A' and str[i] <= 'Z'):
+            upper += 1
+    return upper, lower
+
+
 str = input('Masukkan Kalimat yang ingin dihitung: ')
-upper = 0
-lower = 0
-for i in range(len(str)):
+upper_result, lower_result = check_upper_lower(str)
 
-    if(str[i] >= 'a' and str[i] <= 'z'):
-        lower += 1
-
-    elif(str[i] >= 'A' and str[i] <= 'Z'):
-        upper += 1
-print('Jumlah huruf lower case : ', lower)
-print('Jumlah huruf upper case : ', upper)
+print('Jumlah huruf lower case : ', lower_result)
+print('Jumlah huruf upper case : ', upper_result)
