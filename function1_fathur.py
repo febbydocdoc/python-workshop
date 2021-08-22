d8b25c65d8289df073e812ffe8071f4b7335b047
fathur909 <85476204+fathur909@users.noreply.github.com>
2021-08-22 14:22:41 +0700

    Add files via upload

---
+++ function1_fathur.py
@@ -0,0 +1,17 @@
+string = input("Type Word: ")
+
+
+def lower_case(string):
+    return sum(map(str.islower, string))
+
+
+def upper_case(string):
+    return sum(map(str.isupper, string))
+
+
+lower = lower_case(string)
+upper = upper_case(string)
+
+
+print("Hurug Kecil = ", lower)
+print("Huruf Besar = ", upper)
